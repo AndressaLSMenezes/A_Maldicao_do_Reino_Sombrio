ba1ce234da5fac430bce9ffae01ad184136916d5
igordsBueno <ig.dsbueno@gmail.com>
2023-05-03 21:18:22 -0300

    resolução de bugs

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -6,13 +6,14 @@ from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 from dino_runner.utils.text_utils import draw_message_component
 from dino_runner.components.powerups.power_up_manager import PowerUpManager
 
+
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
-        pygame.screen =  pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
         self.playing = False
         self.running = False
         self.score = 0
@@ -23,8 +24,8 @@ class Game:
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
         self.power_up_manager = PowerUpManager()
-        
-    def execute (self):
+
+    def execute(self):
         self.running = True
         while self.running:
             if not self.playing:
@@ -33,7 +34,7 @@ class Game:
         pygame.display.quit()
         pygame.quit()
 
-    def run (self):
+    def run(self):
         self.playing = True
         self.obstacle_manager.reset_obstacles()
         self.power_up_manager.reset_power_ups()
@@ -49,7 +50,7 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
-            
+
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
@@ -58,11 +59,11 @@ class Game:
         self.power_up_manager.update(self.score, self.game_speed, self.player)
 
     def update_score(self):
-        self.score += 1 
+        self.score += 1
         if self.score % 100 == 0:
             self.game_speed += 5
 
-    def draw (self):
+    def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
@@ -74,38 +75,40 @@ class Game:
         pygame.display.update()
         pygame.display.flip()
 
-    def draw_background (self):
+    def draw_background(self):
         image_width = BG.get_width()
-        self.screen.blit(BG,(self.x_pos_bg, self.y_pos_bg))
-        self.scrreen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
         if self.x_pos_bg <= image_width:
-            self.screen.blit(BG,(image_width+ self.x_pos_bg, self.y_pos_bg))
+            self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
             self.x_pos_bg -= self.game_speed
 
-    def draw_score (self):
+    def draw_score(self):
         draw_message_component(
             f"pontos: {self.score}",
-            self.screen, 
-            pos_x_center = 1000,
-            pos_y_center = 50
+            self.screen,
+            pos_x_center=1000,
+            pos_y_center=50
         )
+
     def draw_power_up_time(self):
         if self.player.has_power_up:
-            time_to_show = round((self.player.power_up_time - pygame.time.get_ticks()/ 1000, 2))
+            time_to_show = round(
+                (self.player.power_up_time - pygame.time.get_ticks() / 1000, 2))
             if time_to_show >= 0:
                 draw_message_component(
                     f"{self.player.type.captalize()} enable for {time_to_show} seconds",
                     self.screen,
-                    font_size= 18,
-                    pos_x_center= 500,
-                    pos_y_center= 40
+                    font_size=18,
+                    pos_x_center=500,
+                    pos_y_center=40
                 )
             else:
                 self.player.has_power_up = False
                 self.player.type = DEFAULT_TYPE
 
-    def handle_events_on_menu (self):
+    def handle_events_on_menu(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
@@ -115,13 +118,14 @@ class Game:
 
     def show_menu(self):
         self.screen.fill((255, 255, 255))
-        half_screen_height = SCREEN_HEIGHT // 2 
+        half_screen_height = SCREEN_HEIGHT // 2
         half_screen_width = SCREEN_WIDTH // 2
         if self.death_count == 0:
             draw_message_component(
-                "precione qualquer tecla para iniciar o jogo",self.screen) 
+                "precione qualquer tecla para iniciar o jogo", self.screen)
         else:
-            draw_message_component("Precione qualquer tecla para reiniciar o jogo", self.screen, pos_y_center=half_screen_height + 140)   
+            draw_message_component("Precione qualquer tecla para reiniciar o jogo",
+                                   self.screen, pos_y_center=half_screen_height + 140)
             draw_message_component(
                 f"sua pontuação: {self.score}",
                 self.screen,
@@ -132,7 +136,8 @@ class Game:
                 self.screen,
                 pos_x_center=half_screen_height - 100
             )
-            self.screen.blit (ICON, (half_screen_width - 40, half_screen_height - 30))
+            self.screen.blit(ICON, (half_screen_width -
+                             40, half_screen_height - 30))
 
         pygame.display.flip()
         self.handle_events_on_menu()
--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,3 +1,4 @@
+
 from dino_runner.utils.constants import BIRD
 from dino_runner.components.obstacles.obstacle import Obstacle
 
