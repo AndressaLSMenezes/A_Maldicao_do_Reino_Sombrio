26d56091ce0be3c5aa0d8a783931cc449f30e60e
Andressa <andressalsmenezes@gmail.com>
2023-05-05 20:25:08 -0300

    Co-authored-by: Elton Vinicius <eltonfelii@users.noreply.github.com>

--- menu.py
+++ menu.py
@@ -1,15 +1,17 @@
 import tkinter as tk
+from dino_runner.components.game import Game
 
 class Menu:
     def __init__(self, master):
         self.master = master
-        master.title("Minha aplicação")
+        master.title("A Maldição do Reino Sombrio: A Saga do Cálice da Luz")
         master.geometry("1100x600")
 
         # Carrega a imagem de fundo
-        self.background_image = tk.PhotoImage(file="fundo-novo.png")
+        self.background_image = tk.PhotoImage(file="dino_runner/assets/Buttons/fundo.png")
         
         # Cria um canvas com a mesma dimensão da janela
+        # Canvas fornece uma área gráfica
         self.canvas = tk.Canvas(master, width=1100, height=600)
         self.canvas.pack()
         
@@ -20,31 +22,22 @@ class Menu:
         self.button_frame = tk.Frame(self.canvas, bd=0)
         self.button_frame.place(relx=0.5, rely=0.5, anchor="center")
         
-        self.image_play = tk.PhotoImage(file="eleonor (1).png").subsample(2, 2)
+        self.image_play = tk.PhotoImage(file="dino_runner/assets/Buttons/play.png").subsample(2, 2)
         self.image_button_play = tk.Button(self.button_frame, image=self.image_play, command=self.person_1, borderwidth=0, highlightthickness=0)
         self.image_button_play.pack(pady=0)
         
-        
-        self.image_credits = tk.PhotoImage(file="novo legolas.png").subsample(2, 2)
-        self.image_button_credits = tk.Button(self.button_frame, image=self.image_credits, command=self.person_2, borderwidth=0, highlightthickness=0)
-        self.image_button_credits.pack(pady=0)
-        
-        self.image_exit = tk.PhotoImage(file="2.png").subsample(2, 2)
+        self.image_exit = tk.PhotoImage(file="dino_runner/assets/Buttons/exit.png").subsample(2, 2)
         self.image_button_exit = tk.Button(self.button_frame, image=self.image_exit, command=self.exit, borderwidth=0, highlightthickness=0)
         self.image_button_exit.pack(pady=0)
 
 
     def person_1(self):
-        print("Eleonor")
-        pass
-    
-    def person_2(self):
-        print("Legolas")
+        root.destroy()
+        game = Game()
+        game.execute()
         
-    
     def exit(self):
         root.destroy() #destroy window
-        print("Sair")
         
         
 
